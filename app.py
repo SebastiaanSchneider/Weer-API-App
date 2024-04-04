6c207319629c32d5f463e1c1faa51bad8e879ee2
Sebastiaan Schneider <sebastiaanschneider@gmail.com>
2024-04-04 11:16:55 +0200

    Werkende versie, eerste opruiming gedaan.

--- app.py
+++ app.py
@@ -10,23 +10,21 @@ from icalendar import Calendar, Event, vCalAddress, vText
 from pathlib import Path
 import pytz
 import os
+import re
 import requests
-
+import uuid
 
 app = Flask(__name__, '/static')
 
 
-# index route met weergave vooruitzicht
+# index route met test weergave
 @app.route('/')
 def get_weer():
 
     data = requests.get("http://api.weatherapi.com/v1/forecast.json?key=007ca694539c40549f8105112242603&q=Purmerend&days=7&aqi=no&alerts=no").json()
-    # print(data['location']['name'])
-    print(data['current']['condition'])
-    print(type(data['current']['condition']))
-    print(data['forecast']['forecastday'][1]['date'])
+
     if data:
-        datum = datetime.datetime.now()
+        datum = datetime.now()
         locatie = data['location']['name']
 
         return render_template("index.html", data=data, datum=datum, 
@@ -34,56 +32,77 @@ def get_weer():
     else:
         return "big sad, no data"
 
+
+# route om de ical te vormen en hosten
 @app.route('/ical')
 def geef_agenda_feed():
-    ical = '''BEGIN:VCALENDAR
-PRODID:-//Google Inc//Google Calendar 70.9054//EN
-VERSION:2.0
-CALSCALE:GREGORIAN
-METHOD:PUBLISH
-X-WR-CALNAME:test
-X-WR-TIMEZONE:Europe/Amsterdam
-BEGIN:VEVENT
-DTSTART:20240403T080000Z
-DTEND:20240403T100000Z
-DTSTAMP:20240403T080705Z
-UID:2c925eh6840mpla95ioc1gkrm4@google.com
-CREATED:20240403T080016Z
-LAST-MODIFIED:20240403T080034Z
-SEQUENCE:0
-STATUS:CONFIRMED
-SUMMARY:Zonnig 20graden
-TRANSP:OPAQUE
-END:VEVENT
-BEGIN:VEVENT
-DTSTART:20240403T100000Z
-DTEND:20240403T110000Z
-DTSTAMP:20240403T080705Z
-UID:30c52im316ahn415bd507uievp@google.com
-CREATED:20240403T080051Z
-LAST-MODIFIED:20240403T080051Z
-SEQUENCE:0
-STATUS:CONFIRMED
-SUMMARY:Kankerheet 36graden
-TRANSP:OPAQUE
-END:VEVENT
-BEGIN:VEVENT
-DTSTART:20240403T110000Z
-DTEND:20240403T130000Z
-DTSTAMP:20240403T080705Z
-UID:500ed7m8kebgu0544j5lu6i4rj@google.com
-CREATED:20240403T080112Z
-LAST-MODIFIED:20240403T080112Z
-SEQUENCE:0
-STATUS:CONFIRMED
-SUMMARY:Helemaal kutweer 5 graden
-TRANSP:OPAQUE
-END:VEVENT
-END:VCALENDAR
+    # importeer api data
+    data = requests.get(
+        "http://api.weatherapi.com/v1/forecast.json?key=007ca694539c40549f8105112242603&q=Purmerend&days=7&aqi=no&alerts=no").json()
 
-'''
+    # init the calendar
+    cal = Calendar()
 
-    return Response(ical, mimetype='text/calendar')
+    # Some properties are required to be compliant
+    cal.add('prodid', '-//My calendar product//example.com//')
+    cal.add('version', '2.0')
+
+    for day in data['forecast']['forecastday']:
+        for hour in day['hour']:
+            # opzet componenten
+            event = Event()
+            start = hour['time']
+            
+            delimiters = ["-", " ", ":"]
+            for delimiter in delimiters:
+                start = " ".join(start.split(delimiter))
+            start = start.split()
+            year = int(start[0])
+            month = int(start[1])
+            day = int(start[2])
+            time_hour = int(start[3])
+            time_minute = int(start[4])
+ 
+            uid = str(uuid.uuid4())
+
+            # combinatie weerbericht
+            regen_kans = str(hour['chance_of_rain']) + "%"
+            text = hour['condition']['text']
+            dew_point = str(hour['dewpoint_c']) + "C"
+            gevoelstemperatuur = str(hour['feelslike_c']) + "C"
+            windvlagen = str(hour['gust_kph']) + "km/h"
+            humidity = str(hour['humidity']) + "%"
+            neerslag = str(hour['precip_mm']) + "mm"
+            luchtdruk = str(hour['pressure_mb']) + "mB"
+            temperature = str(hour['temp_c']) + "C"
+            uv = str(hour['uv'])
+            zicht = str(hour['vis_km']) + "km"
+            windrichting = hour['wind_dir']
+            wind_snelheid = str(hour['wind_kph']) + "km/h"
+            wind_temp = str(hour['windchill_c']) + "C"
+            weerbericht = ("Vooruitzicht: " + text + ", kans op regen: " + 
+                           regen_kans + ", dew_point: " + dew_point + 
+                           ", gevoelstemperatuur: " + gevoelstemperatuur + 
+                           ", windvlagen: " + windvlagen + ", humidity: " + 
+                           humidity + ", neerslag: " + neerslag + 
+                           ", luchtdruk: " + luchtdruk + ", temperature: " + 
+                           temperature + ", uv: " + uv + ", zicht: " + zicht + 
+                           ", windrichting: " + windrichting + 
+                           ", wind_snelheid: " + wind_snelheid + 
+                           ", wind_temp: " + wind_temp)
+
+            # aanmaak componenten
+            event.add('dtstart', datetime(year, month, day, time_hour, 
+                                          time_minute, 0, tzinfo=pytz.timezone('Europe/Amsterdam')))
+            event.add('dtend', datetime(year, month, day,
+                      (time_hour + 1) % 24, time_minute, 0, tzinfo=pytz.timezone('Europe/Amsterdam')))
+            event['uid'] = uid
+            event.add('summary', weerbericht)
+            event.transparent = False
+
+            # toevoegen componeneten aan cal
+            cal.add_component(event)
+    return Response(cal.to_ical(), mimetype='text/calendar')
 
 
 if __name__ == '__main__':
